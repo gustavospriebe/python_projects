4e961c3f0f7e489b240320266b90ddb79a721c4f
Gustavo Priebe <gustavo_priebe@hotmail.com>
2021-12-08 16:18:27 -0300

    update lambda function

--- phrase_maker.py
+++ phrase_maker.py
@@ -2,8 +2,7 @@ lista = []
 
 # This program receive user inputs and transform them into a phrase. Int isn't accepted.
 
-
-def phrase(x):  # Capitalizes the first letter of the sentence.
+def phrase(x): # Capitalizes the first letter of the sentence.
     capitalize = str(x).capitalize()
     if x.startswith(('how', 'what', 'why', 'who')):
         return f'{capitalize}?'
@@ -11,7 +10,7 @@ def phrase(x):  # Capitalizes the first letter of the sentence.
         return f'{capitalize}.'
 
 
-def loop():  # Loop through the user inputs and put . If the \end is inserted, then break the program and return the result.
+def loop(): # Loop through the user inputs and put . If the \end is inserted, then break the program and return the result.
     while True:
         user_input = input('Say something: ')
         try:
@@ -19,11 +18,9 @@ def loop():  # Loop through the user inputs and put . If the \end is inserted, t
             continue
         except ValueError:
             if user_input == '\end':
-                # Join the phrases into a sentence.
-                def final(x): return print(' '.join(x))
+                final = lambda x: print(' '.join(x)) # Join the phrases into a sentence.
                 final(lista)
                 break
             lista.append(phrase(user_input))
 
-
-loop()  # Inicialize the program.
+loop() # Inicialize the program.
