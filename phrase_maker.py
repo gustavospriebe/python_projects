e56f5370ce86a061df488fb02ae063092419fa2e
Gustavo Priebe <gustavo_priebe@hotmail.com>
2021-12-08 11:44:02 -0300

    phrase maker project done

---
+++ phrase_maker.py
@@ -0,0 +1,29 @@
+lista = []
+
+# This program receive user inputs and transform them into a phrase. Int isn't accepted.
+
+def phrase(x): # Capitalizes the first letter of the sentence.
+    capitalize = str(x).capitalize()
+    if x.startswith(('how', 'what', 'why', 'who')):
+        return f'{capitalize}?'
+    else:
+        return f'{capitalize}.'
+
+
+def loop(): # Loop through the user inputs and put . If the \end is inserted, then break the program and return the result.
+    while True:
+        user_input = input('Say something: ')
+        try:
+            user_input = int(user_input)
+            continue
+        except ValueError:
+            if user_input == '\end':
+                final(lista)
+                break
+            lista.append(phrase(user_input))
+
+
+def final(x): # Join the phrases into a sentence.
+    print(' '.join(x))
+
+loop() # Inicialize the program.
